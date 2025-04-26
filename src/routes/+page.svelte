066be74039840d29730f481831a8828eb905cf6e
GHOST <ghostdevbusiness@gmail.com>
2025-04-26 17:31:20 +0100

    feat: add turtle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { CldImage } from 'svelte-cloudinary';
+</script>
+
+<CldImage src="images/turtle" alt="Turtle" width={960} height={600} />
